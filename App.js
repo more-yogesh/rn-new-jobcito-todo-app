4f59a88cb4bc6c9745ecfb2ed3f7d59e681763da
Yogesh <yogeshmore044@gmail.com>
2020-10-16 11:59:09 +0530

    initial map app

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+	const [textInput, setChangeText] = useState('');
+
+	const onChangeTextHandler = (inputText) => {
+		setChangeText(textInput => inputText);
+	}
+
+	const [taskItems, setTaskItems] = useState([]);
+
+	const onPressHandler = () => {
+		setTaskItems(taskItems => [...taskItems, textInput])
+		console.log(taskItems);
+	};
+
+
+
+
+
+	return (
+		<View style={design.container}>
+			<View style={design.form}>
+				<View style={{ flex: 3 }}>
+					<TextInput placeholder="Enter Task" style={design.inputControl} onChangeText={onChangeTextHandler} />
+				</View>
+				<View style={{ flex: 1 }}>
+					<Button style={design.btn} title="Add New Task" onPress={onPressHandler} />
+				</View>
+			</View>
+			<View>
+				{
+					taskItems.map((value, index) => {
+						return (
+							<View key={index} style={design.taskItem}>
+								<Text>{value}</Text>
+							</View>
+						);
+					})
+				}
+			</View>
+		</View>
+	);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const design = StyleSheet.create({
+	container: {
+		margin: 50
+	},
+	inputControl: {
+		borderWidth: 2,
+		borderColor: 'black',
+		padding: 6,
+		marginRight: 15
+	},
+
+	form: {
+		flexDirection: 'row'
+	},
+	taskItem: {
+		backgroundColor: 'gray',
+		color: 'white',
+		padding: 5,
+		borderColor: 'black',
+		borderWidth: 3,
+		marginVertical: 5
+
+	}
 });
